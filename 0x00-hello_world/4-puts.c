fc0762f3db8b26101cf49d0c6f56df41a61b2b9c
VictorMaxima <victormaxchidera@gmail.com>
2023-02-17 14:00:26 -0800

    C_hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include "string.h"
+#include <stdio.h>
+/*
+* 4-puts.c
+*entry point main
+*/
+
+int main(void)
+	{
+	/*
+	 * entry point
+	 */
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+Learn8ng Low level programming at ALX
