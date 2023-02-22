50f5bb22300d080aaa4887c8819b1015f816384a
VictorMaxima <victormaxchidera@gmail.com>
2023-02-22 12:05:04 -0800

    Fix6

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	for (i = 0; i < 25; i++)
+	for (i = 0; i < 24; i++)
 	{
-		for (j = 0; j < 61; j++)
+		for (j = 0; j < 60; j++)
 		{
 			_putchar((i / 10) + 48);
 			_putchar((i % 10) + 48);
