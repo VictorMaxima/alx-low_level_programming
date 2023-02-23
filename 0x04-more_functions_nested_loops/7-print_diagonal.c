a8c046184f178ce305c42b7cab22a4e5aea4ff51
VictorMaxima <victormaxchidera@gmail.com>
2023-02-23 13:37:55 -0800

    More Functions

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_diagonal - draws a diagonal
+* @n: how many lines to print
+* Return: no return
+*/
+
+void print_diagpnal(int n)
+{
+	int m;
+
+	for (m = 1; m <= n; m++)
+	{
+		while (m > 1)
+		{
+			_putchar(' ');
+		}
+		_putchar('\');
+		_putchar(10);
+	}
+	if (n < 1)
+		_putchar(10);
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_square - pronts a square
+* @size: size of the square
+* Return: No Return
+*/
+void print_square(int size)
+{
+	int m = 0;
+	int o;
+
+	if (n < 1)
+		_putchar(10);
+	while (m < n)
+	{
+		for (o = 0; o < n; o++)
+		{
+			_putchar('#')
+		}
+		_putchar(10);
+	}
+
